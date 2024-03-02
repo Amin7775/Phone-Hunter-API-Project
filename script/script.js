4c99485960499286d6feda4b93850a127ea36ffa
Amin7775 <amin.dpi24@gmail.com>
2024-03-02 18:05:40 +0600

    Implement Search Functionality And Display Search Result

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 // fetch
-const loadPhone = async () => {
+const loadPhone = async (searchText) => {
   const response = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await response.json();
   const phone = data.data;
@@ -13,18 +13,20 @@ loadPhone();
 
 // display
 const displayPhones = (phones) => {
+    // get container id
+    let container = document.getElementById('phone-container')
+    container.innerHTML=''
   phones.forEach((phone) => {
     console.log(phone);
 
-    // get container id
-    let container = document.getElementById('phone-container')
     const div = document.createElement("div");
     div.classList = `card w-full bg-base-100 border border-white/[.15] shadow-xl`;
     div.innerHTML = `
-        <figure><img class="rounded-2xl mt-10" src="${phone.image}" alt="Shoes" /></figure>
+    <figure><img class="rounded-2xl mt-10" src="${phone.image}" alt="Shoes" /></figure>
                 <div class="card-body">
-                  <h2 class="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <h2 class="card-title">${phone.phone_name}</h2>
+                  <p>Brand : ${phone.brand}</p>
+                  <p class = "pb-5">Model : ${phone.slug}</p>
                   <div class="card-actions justify-end">
                     <button class="btn btn-primary">Buy Now</button>
                   </div>
@@ -34,3 +36,13 @@ const displayPhones = (phones) => {
     container.appendChild(div)
   });
 };
+
+
+// btn
+function handleSearch(){
+    const searchField = document.getElementById('search-field')
+    const searchFieldText = searchField.value;
+    console.log(searchFieldText)
+    loadPhone(searchFieldText)
+    
+}
